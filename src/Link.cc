3b3346ed8ad4e8f379bd2a41a36b5d698dda605c
s-sharar <ssharar@uwaterloo.ca>
2024-11-16 15:44:14 -0500

    Link class

--- src/Link.cc
+++ src/Link.cc
@@ -2,4 +2,61 @@
 #include <sstream>
 
 
+Link::Link(char id, LinkType t, int strength, int row, int col) : 
+    id{id}, t{t}, strength{strength}, row{row}, col{col}, stepSize{1}, isVisible{false}, isDownloaded{false} {}
 
+char Link::getId() const {
+    return id;
+}
+
+LinkType Link::getType() const {
+    return t;
+}
+
+int Link::getStrength() const {
+    return strength;
+}
+
+int Link::getRow() const {
+    return row;
+}
+
+int Link::getCol() const {
+    return col;
+}
+
+bool Link::getIsVisible() const {
+    return isVisible;
+}
+
+bool Link::getIsDownloaded() const {
+    return isDownloaded;
+}
+
+void Link::setRow(int r) {
+    if (!isDownloaded) row = r;
+}
+
+void Link::setCol(int c) {
+    if (!isDownloaded) col = c;
+}
+void Link::setStrength(int s) {
+    if (!isDownloaded) strength = s;
+}
+
+void Link::linkBoost() {
+    stepSize += 1;
+}
+
+void Link::polarise() {
+    t = (t == Data) ? Virus : Data;
+}
+
+void Link::scan() {
+    isVisible = true;
+}
+
+void Link::download() {
+    isDownloaded = true;
+}
+    
